d0281c62cf857a8b4ec331cb4a0fffec980fa7dd
Kirsten Segavac <kirstensegavac@gmail.com>
2022-05-16 14:33:06 +0800

    create new category in category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,16 +1,15 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
+const { findAll, create } = require('../../models/Category');
 
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
   // find all categories
   Category.findAll({
-      // be sure to include its associated Products
+    // be sure to include its associated Products
     include: [
-      {
-    model: Product
-      }
+      { model: Product }
     ]
   }).then((categories) => {
     res.json(categories);
@@ -19,15 +18,28 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findByPk(req.params.id, {
+      // be sure to include its associated Products
+    include: [
+      { model: Product }
+    ]
+  }).then((categories) => {
+    res.json(categories);
+  });
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    name: req.body.name
+  }).then((created) => {
+    res.json(created);
+  })
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+Category.update(id)
 });
 
 router.delete('/:id', (req, res) => {
