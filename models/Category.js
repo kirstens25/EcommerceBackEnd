4e2ea5a981b03d71622b9c38a7627461f0672451
Kirsten Segavac <kirstensegavac@gmail.com>
2022-06-04 18:38:57 +0800

    fixed all post requests

--- models/Category.js
+++ models/Category.js
@@ -7,6 +7,12 @@ class Category extends Model { }
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false
